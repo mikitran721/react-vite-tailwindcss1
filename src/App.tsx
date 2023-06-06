2c0c4d2e21f032787135e93aadf4db3cf74e8ead
MikiTran <mikitran721@gmail.com>
2023-06-06 23:40:07 +0700

    hoan thien navba va buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
 import "./App.css";
+import AuthButtons from "./components/AuthButtons";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <>
-      <h1 className="text-5xl font-extrabold">HELLO WORLD</h1>
+      <div className="grid grid-cols-5">
+        <Nav />
+        <main className="px-12 py-6 col-span-4 bg-cyan-50">
+          <AuthButtons />
+          MAIN CONTENT Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Repudiandae obcaecati incidunt eum tempora perspiciatis quis quaerat
+          odit laboriosam voluptatem, itaque, temporibus molestiae, fugiat
+          distinctio non natus hic dignissimos eveniet? Pariatur? Lorem Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+          exercitationem sunt amet similique minus rem provident aliquam
+          perspiciatis dolorum cupiditate, rerum magni qui eius quo odit est
+          possimus pariatur eum.
+        </main>
+      </div>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        barLow:['"Barlow Condensed"','tahoma'],
+      },
+      colors:{
+        primary:'#14605B',
+      }
+    },
   },
   plugins: [],
 }
